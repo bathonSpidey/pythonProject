f5e237656fa3d07faa94804f5f2c58f510bf5d84
Abir Bhattacharyya <bathonbhats@gmail.com>
2022-12-06 17:33:33 +0100

    added opencv viewer to get in real time view

--- SaveFilesToPly.py
+++ SaveFilesToPly.py
@@ -1,4 +1,5 @@
 import pyrealsense2 as rs
+import numpy as np
 
 
 def savePly():
@@ -9,14 +10,21 @@ def savePly():
     config.enable_stream(rs.stream.depth)
     pipe.start(config)
     colorizer = rs.colorizer()
+    print(points)
 
     try:
         frames = pipe.wait_for_frames()
+        aligned_depth_frame = frames.get_depth_frame()
+        points = point_cloud.calculate(aligned_depth_frame)
+        print(points)
+        vertices = np.asanyarray(points.get_vertices(dims=2))
+        print(vertices)
+        np.savetxt("test.txt", vertices)
         colorized = colorizer.process(frames)
         ply = rs.save_to_ply("saved.ply")
         ply.set_option(rs.save_to_ply.option_ply_binary, True)
         ply.set_option(rs.save_to_ply.option_ply_normals, True)
-        ply.process(colorized)
+        processed = ply.process(colorized)
         print("Done")
     finally:
         pipe.stop()
